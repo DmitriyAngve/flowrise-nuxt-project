97e757d6f53c45a072917a4efbfcde3aac4f9658
DmitriyAngve <kirirch129@gmail.com>
2023-10-16 15:15:20 +0700

    #

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,12 @@
+<!-- Определяю переменные и логику в блоке <script setup>. Этот блок обеспечивает удобство при работе с композицией в компонентах -->
+<script setup lang="ts">
+// Это пользовательский хук, определённый в "useSettings.ts"
+const settings = useSettings();
+</script>
+
 <template>
-  <header>header</header>
+  <Bounded as="header">
+    <Logo />
+    {{ settings }}
+  </Bounded>
 </template>
