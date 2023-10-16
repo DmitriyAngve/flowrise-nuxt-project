2a085ea50214f408588ba71711f2905d7e35d513
DmitriyAngve <kirirch129@gmail.com>
2023-10-16 17:03:38 +0700

    feat: header and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 const settings = useSettings();
 
-const { asImageSrc } = usePrismic();
+const { asImageSrc } = usePrismic(); // Использую этот хук и сразу деструктурирую результат, чтобы получить ф-ию для преобразования данных в URL изображения
 
 useSeoMeta({
   title: settings.value?.data.site_title,
   description: settings.value?.data.meta_description,
   ogImage: asImageSrc(settings.value?.data.og_image),
-});
+}); // В этой строке вызывается хук, который предназначен для установки мета-тегов SEO для веб-страницы.
 </script>
 
 <template>
