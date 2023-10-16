d24358f0378332f9955c9785c437ad3de4e72111
DmitriyAngve <kirirch129@gmail.com>
2023-10-16 15:59:02 +0700

    #

--- components/Bounded.vue
+++ components/Bounded.vue
@@ -4,7 +4,9 @@ defineProps<{ as?: string }>();
 
 <!--   <Component :is="as || 'section'"></Component> - это пример использования динамического компонента. В этом примере устанавливается компонент, который должен быть отображен, с использованием атрибута ":is" с помощью которого можно определить, какой компонент будет использоваться. ":is" - это директива связывания данных, которая используется для определения того, какой компонент будет отображен -->
 <template>
-  <Component :is="as || 'section'" class="px-4 md:px-6">
-    <slot />
+  <Component :is="as || 'section'" class="px-4 py-10 md:py-14 md:px-6 lg:py-16">
+    <div class="max-w-6xl w-full mx-auto">
+      <slot />
+    </div>
   </Component>
 </template>
