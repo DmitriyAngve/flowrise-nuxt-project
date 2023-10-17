cc913c4c8c554841572b2be723932786e52a177c
DmitriyAngve <kirirch129@gmail.com>
2023-10-17 14:01:12 +0700

    #

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import { components } from '~/slices'
+import { components } from "~/slices";
 
-const prismic = usePrismic()
-const { data: page } = useAsyncData('index', () =>
-  prismic.client.getByUID('page', 'home')
-)
+const prismic = usePrismic();
+const { data: page } = useAsyncData("index", () =>
+  prismic.client.getByUID("page", "home")
+);
 
-useHead({
-  title: prismic.asText(page.value?.data.title)
-})
+useSeoMeta({
+  // Устанавливаю заголовок страницы на основе "page.value?.data.meta_title". Это для управления мета тегами SEO для каждой страницы в приложении.
+  title: page.value?.data.meta_title ?? undefined,
+  description: page.value?.data.meta_description ?? undefined,
+  ogImage: prismic.asImageSrc(page.value?.data.meta_image) ?? undefined,
+  // OG - Open Graphю
+  // В целом этот код обеспечивает правильное заполнение мет-тегов SEO (заголовка, описания, изображения) на основе данных, предоставленных в объекте "page" и хуке "prismic"
+});
 </script>
-
 
 <template>
   <SliceZone
