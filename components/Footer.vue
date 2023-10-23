306455fd8a240b4de655d986de7e1c4437153f3d
DmitriyAngve <kirirch129@gmail.com>
2023-10-23 19:26:07 +0700

    #

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,25 +1,25 @@
-<!-- Определяю переменные и логику в блоке <script setup>. Этот блок обеспечивает удобство при работе с композицией в компонентах -->
-<script setup lang="ts">
-// Это пользовательский хук, определённый в "useSettings.ts"
+<script lang="ts" setup>
 const settings = useSettings();
 </script>
 
 <template>
-  <Bounded as="footer" class="!py-4 md:!py-6 lg:!py-8 bg-white">
-    <div class="flex gap-4 items-center justify-between flex-col sm:flex-row">
-      <Logo />
+  <Bounded as="footer">
+    <div class="flex sm:flex-row flex-col justify-between items-center gap-6">
+      <NuxtLink to="/">
+        <Logo />
+      </NuxtLink>
+
       <p class="text-xs">
         ©{{ new Date().getFullYear() }} {{ settings?.data.site_title }}
       </p>
-      <nav>
-        <ul class="flex">
-          <li v-for="item of settings?.data.navigation" :key="item.label ?? ''">
-            <PrismicLink :field="item.link" class="p-4">
-              {{ item.label }}
-            </PrismicLink>
-          </li>
-        </ul>
-      </nav>
+
+      <ul class="flex">
+        <li v-for="item of settings?.data.navigation" :key="item.label ?? ''">
+          <PrismicLink :field="item.link" class="p-3">
+            {{ item.label }}
+          </PrismicLink>
+        </li>
+      </ul>
     </div>
   </Bounded>
 </template>
